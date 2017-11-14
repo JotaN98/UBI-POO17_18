d36af328ebff23f76469a8cfee226dbccd79c7f5
BrunoTavares25 <bruno.tavares250@gmail.com>
2017-11-14 21:53:12 +0000

    kraken teste

--- Aluno.java
+++ Aluno.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package projeto;
 
 import java.text.DateFormat;
 
-/**
- *
- * @author BrunoTavares
- */
 public class Aluno extends Pessoa {
     private int numero;
     private int ano;
