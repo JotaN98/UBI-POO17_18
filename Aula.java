f778bb9be93dc95ecf42c829530d0a7d8d5a1793
JotaN <jndomingos1998@gmail.com>
2017-11-30 20:43:17 +0000

    Aula - Fiz os statics os getters e os setters
    Falta o métodos equals e clone

--- Aula.java
+++ Aula.java
@@ -4,71 +4,116 @@ import javafx.scene.input.DataFormat;
 import java.util.Map;
 import java.util.HashMap;
 
-public class Aula {
-    private Disciplina disciplina;
-    private Date data;
-    private ArrayList<Aluno> aluno;
-    private Date CodeID;
+public class Aula extends Entity {
+    // -- beginning of static fields
+    // -- vars
+    private static final Map<String, Aula> aulas = new HashMap<String, Aula>();
+    private static long IDCount = 0;
     
-    public static boolean addAula(Aula x,Date horarioNovo){
-        if(existeAula(x)){
-            if(x.getHorario().equals(horarioNovo)){
-                System.out.println("Essa aula já existe nesse horário");
-                return false;
-            }
+    public static Aula getAulaFromID(long ID){
+        return aulas.get(Entity.getGroupIDFromGroup("Aula") + ID );
+    }
+    
+    public static boolean addAula(Aula x){
+        if(getAulaFromID(x.getID())!=null){
+            System.out.println("Aula existente");
+            return false;
         }
-        Aulas.put(x.getCodeID(), x); 
-        System.out.println("Aula reposta");
+        aulas.put(x.getCodeID(), x);
         return true;
     }
-    
-    public static boolean existeAula(Aula x){
-        for(int i=0;i<Aulas.size();i++){
-            if(x.equals(Aulas.get(i)))
-                return true;
-        }
-        return false;
+   
+    public static long Create(int hora[], long prof, long disciplina, long turma, String sala){
+        Aula nAula = new Aula(hora[], prof, disciplina, turma, sala);
+        addAula(nAula);
+        return nAula.getID();
     }
     
-    private static final Map<String, Aula> Aulas = new HashMap<String, Aula>();
+    // -- beginning of non static fields
+    // -- vars
+    private int hora[];
+    private long prof;
+    private long disciplina;
+    private long turma;
+    private String sala;
+    
+     // -- constructors
+    public Aula(int hora[], long prof, long disciplina, long turma, String sala){
+        super("Aula", IDCount++);
+        this.hora=hora;
+        this.prof=prof;
+        this.disciplina=disciplina;
+        this.turma=turma;
+        this.sala=sala;
+    }
+    
+    // -- methods
+    public int[] getHora() {
+        return hora;
+    }
 
-    public Disciplina getDisciplina() {
+    public void setHora(int[] hora) throws IllegalArgumentException {
+        for(int i=0; i<=hora.length; i++){
+            if(hora[i]<8 || hora[i]>18){
+                System.out.println("As aulas têm de estar compreendidas entre as 8 e as 18 horas");
+                throw new IllegalArgumentException();}
+        }
+        this.hora = hora;
+    }
+
+    public long getProf() {
+        return prof;
+    }
+
+    public void setProf(long prof) throws IllegalArgumentException {
+        if(Professor.getProfessorFromID(prof)!=null){
+            this.prof=prof;
+        }
+        else{
+            System.out.println("Professor ID \"" + prof + "\"não foi encontrado.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public long getDisciplina() {
         return disciplina;
     }
 
-    public Date getData() {
-        return data;
-    }
-
-    public ArrayList<Aluno> getAluno() {
-        return aluno;
-    }
-
-    public void setDisciplina(Disciplina disciplina) {
+    public void setDisciplina(long disciplina) throws IllegalArgumentException {
+        if(Disciplina.getDisciplinaFromID(disciplina)!=null){
         this.disciplina = disciplina;
+        }
+        else{
+            System.out.println("Disciplina ID \"" + disciplina + "\" não foi encontrado.");
+            throw new IllegalArgumentException();
+        }
     }
 
-    public void setData(Date data) {
-        this.data = data;
+    public long getTurma() {
+        return turma;
     }
 
-    public void setAluno(ArrayList<Aluno> aluno) {
-        this.aluno = aluno;
+    public void setTurma(long turma)throws IllegalArgumentException {
+        if(Turma.getTurmaFromID(turma)!=null){
+        this.turma = turma;
+        }
+        else{
+            System.out.println("Turma ID \"" + turma + "\" não foi encontrado.");
+            throw new IllegalArgumentException();
+        }
     }
 
-    public static Map<String, Aula> getAulas() {
-        return Aulas;
+    public String getSala() {
+        return sala;
     }
 
-    public Date getCodeID() {
-        return CodeID;
+    public void setSala(String sala){
+        this.sala = sala;
     }
 
-    public void setCodeID(Date horario) {
-        this.CodeID = CodeID;
-    }
-
+    @Override
     public String toString() {
-        return "Aula{" + "disciplina=" + disciplina + ", data=" + data + ", aluno=" + aluno + '}';
-    }    
+        return getCodeID() + " Hora [" + hora + "] " + " Professor: "+ prof + " Disciplina: " + disciplina + " Turma: " + turma + " Sala: " + sala ;
+    }
+    
 }
