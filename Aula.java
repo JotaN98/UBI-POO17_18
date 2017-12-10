799673caf1d1130976b5dd761ac5240178ad0233
BrunoTavares25 <bruno.tavares250@gmail.com>
2017-12-10 12:20:59 +0000

    Revert "dei pull"
    This reverts commit deb3d6137d893ee2b4f67ee87809d4ec75695c6d.

--- Aula.java
+++ Aula.java
@@ -16,10 +16,6 @@ public class Aula extends Entity {
         Create();
     }
     
-    public static Map<String, Aula> getAulas(){
-        return aulas;
-    }
-    
     //Initialize Hours and Days of the week converters
     static{
         ConversorHoras = new ArrayList<String>();
@@ -37,13 +33,7 @@ public class Aula extends Entity {
         ConversorDiaDaSemana.add("Sexta-feira");
     }
     
-     public static Aula getAulaFromID(long ID){
-        return aulas.getOrDefault(
-                Entity.getGroupFromID("Aula") + ID,
-                aulas.get(Entity.getGroupFromID("Aula") + "0")
-        );
-    }  
-    
+            
     public static Aula getAulaFromID(Entity ID){
         return aulas.getOrDefault(
                 ID.getCodeID(),
