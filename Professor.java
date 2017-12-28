364b2dfae931ed661bbed667473f9260f28663fd
Unknown <ranguna313616@hotmail.com>
2017-12-28 16:06:26 +0000

    Corrigi o error do Initializer

--- Professor.java
+++ Professor.java
@@ -92,6 +92,7 @@ public class Professor extends Pessoa{
 		super("Professor", IDCount++, "", "", null);
 		aulas=new ArrayList<Entity>();
 		testes=new ArrayList<Entity>();
+		horario = new ArrayList<ArrayList<Entity>>();
 
 		for(int dia=0; dia<Aula.ConversorDiaDaSemana.size();dia++){
 			horario.add(new ArrayList<Entity>());
